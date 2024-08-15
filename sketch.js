7dabe03e105c381cfa7e5cb1bc3f4c84fca52a3d
vkzx7 <victor.walachy@escola.pr.gov.br>
2024-08-15 10:33:25 -0300

    adicionei codigos index, sketch e style

---
+++ sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    
+    background(255,0,0,0.57)
+    
+    stroke("#00FF0A")
+    fill ("#00BCD4") ;
+    
+    console.log(mouseIsPressed);
+      stroke("#000000");
+    fill("#0027FF");
+  if (mouseIsPressed){
+    rect (mouseX, mouseY, 10, 15);
+  }
+  function setup() {
+    // cria uma tela de 400px de largura por 400px de altura
+    createCanvas(400, 400);
+  }
+  
+    function setup() {
+    createCanvas(400, 400);
+    background("#E91E63")
+  }
+  
+  function draw() {
+  
+  }  
+    
+    if (mouseIsPressed) {
+      circle(mouseX, mouseY, 20, 50);
+    }
+  }
+  
+  
